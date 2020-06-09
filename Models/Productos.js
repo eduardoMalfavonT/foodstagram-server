1bc20e0697a1db29be2f7aa67a18cc87448dec53
Metoosky00 <malfavontalaveraj@hotmail.com>
2020-06-09 12:37:03 -0500

    ingredientes
    se agrego un apartado de ingredientes al modelo para su proxima utilizacion en la pagina foodstagram

--- Models/Productos.js
+++ Models/Productos.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const productosSchema = new Schema({
   nombre: { type: String },
   precio: { type: Number, trim: true },
+  ingrediantes: [{ type: String }],
   categoria: { type: String },
   direccion: { type: String },
   imagen: { type: String },
